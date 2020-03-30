643b60b7bbba9aac58512d62be5eca66f6c9672d
Lauro Tremea Culau <laurotc@gmail.com>
2020-03-29 21:47:19 -0300

    Creates validations and tests in the backend
    Signed-off-by: Lauro Tremea Culau <laurotc@gmail.com>

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const config = require('../../knexfile.js');
 
-const connection = knex(config.development);
+const db_config = process.env.NODE_ENV === 'test' ? config.test : config.development;
+
+const connection = knex(db_config);
 
 module.exports = connection;
